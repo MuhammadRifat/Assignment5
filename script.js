2cc304f68fe5d34c291322026d3224f86a5c8598
Muhammad Rifat <hrifat450@gmail.com>
2021-02-07 22:44:02 +0600

    Completed

---
+++ script.js
@@ -0,0 +1,80 @@
+// For getting meals after search
+function getMeal(){
+    const mealName = document.getElementById('input-meal').value;
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s='+ mealName +'')
+    .then(response => response.json())
+    .then(data => {
+        if(data.meals === null){        //checking meals array is empty or not
+            document.getElementById('message-notFound').style.display = "block";
+            document.getElementById('message-notFound').innerText = "No Meals Found";
+            document.getElementById('ingredients').style.display = "none";
+            clearAllMeals();
+        }
+        else{
+            document.getElementById('message-notFound').style.display = "none";
+            document.getElementById('ingredients').style.display = "none";
+            clearAllMeals();
+            clearAllIngredients();
+            displayMeals(data);
+        }
+    });
+}
+
+// For displaying all meals after search
+const displayMeals = mealsOb => {
+    const mealArray = mealsOb.meals;
+    const mealContainer = document.getElementById('container');
+    mealArray.forEach(meal => {
+        const mealDiv = document.createElement('div');
+        mealDiv.className = 'col-md-3';
+        const mealInfo = `
+            <button onclick="getIngredients('${meal.idMeal}')" class="btn mt-4">
+                <div class="meal text-center">
+                    <img class="w-100 rounded" src="${meal.strMealThumb}">
+                    <h3 class="p-3">${meal.strMeal}</h3>
+                </div>
+            </button>
+        `;
+        mealDiv.innerHTML = mealInfo;
+        mealContainer.appendChild(mealDiv);
+    });
+}
+
+// For getting all ingredients after select any meal
+const getIngredients = mealId => {
+    fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i='+ mealId +'')
+    .then(response => response.json())
+    .then(data => displayIngredients(data));
+}
+
+// For displaying all ingredients after select any meal
+const displayIngredients = meal => {
+    const mealArray = meal.meals[0];
+    document.getElementById('ingredients').style.display = "block";
+    document.getElementById('meal-image').src = mealArray.strMealThumb;
+    document.getElementById('meal-name').innerText = mealArray.strMeal;
+    
+    clearAllIngredients();
+    const ul = document.getElementById('list');
+    for(let i = 1; i <= 20; i++){
+        let ingredients = "strIngredient" + i;
+        if(mealArray[ingredients].length == 0){
+            continue;
+        }
+        else{
+            let li = document.createElement("li");
+            li.innerText = mealArray[ingredients];
+            ul.appendChild(li);
+        }
+    }
+}
+
+// For clearing all existing meal
+function clearAllMeals(){
+    document.getElementById("container").innerText = "";
+}
+
+// For clearing all existing ingredients
+function clearAllIngredients(){
+    document.getElementById('list').innerText = "";
+}
